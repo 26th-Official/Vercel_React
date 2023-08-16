f6f518623460a1cefaba64e99f84fcd23c893939
26th-Official <you@example.com>
2023-08-16 17:18:28 +0530

    Testing 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Samplecomponent from './Samplecomponent'
+import Samplecomponent from './Components/Samplecomponent'
 
 function App() {
   const [count, setCount] = useState(0)
